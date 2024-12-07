fb51828799e42ed5b22e8f15a72bd261e6b55c04
cabral <gustavocabral97@hotmail.com>
2024-12-07 11:13:16 -0300

    exercicio 2 e 3

--- src/main/java/Carro.java
+++ src/main/java/Carro.java
@@ -1,16 +1,45 @@
 public class Carro {
-  String marca;
-  String modelo;
-  int ano;
+  private String marca;
+  private String modelo;
+  private int ano;
+  static  int contador = 0;
   public Carro (String marca, String modelo, int ano){
     this.marca = marca;
     this.modelo = modelo;
     this.ano = ano;
+    Carro.contador += 1;
   }
 
   public String info() {
     return "Marca: %s, Modelo: %s, Ano: %d".formatted(
         this.marca, this.modelo, this.ano
     );
+  }
+
+  public String getMarca() {
+    return marca;
+  }
+
+  public void setMarca(String marca) {
+    this.marca = marca;
+  }
+
+  public String getModelo() {
+    return modelo;
+  }
+
+  public void setModelo(String modelo) {
+    this.modelo = modelo;
+  }
+
+  public int getAno() {
+    return ano;
+  }
+
+  public void setAno(int ano) {
+    this.ano = ano;
+  }
+  static int quantidadeDeCarros() {
+    return Carro.contador;
   }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,12 @@ public class Main {
     Carro carro = new Carro("Hyundai", "HB20", 2022);
 
     System.out.println(carro.info());
+    System.out.println("contagem de carros: " + Carro.quantidadeDeCarros());
+    Carro carro2 = new Carro("Volkswagen", "Golf", 2001);
+
+    System.out.println(carro2.info());
+    System.out.println("contagem de carros: " + Carro.quantidadeDeCarros());
+
     System.out.println("Finalizando sistema...");
   }
 }
